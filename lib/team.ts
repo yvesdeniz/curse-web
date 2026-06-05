ba65c542a7aea6b873d69c6e760d6ee7b0d17fce
yvesdeniz <saint@afra.id>
2026-06-05 18:30:54 +0400

    feat: update team details

--- lib/team.ts
+++ lib/team.ts
@@ -15,9 +15,19 @@ export const team: TeamMember[] = [
   {
     name: "yvesdeniz",
     role: "Founder & Lead Developer",
+    avatar: "https://api.alo.ne/file/i6b1zb",
     socials: {
+      discord: "https://discord.com/users/1306505500699000856",
       github: "https://github.com/yvesdeniz",
     },
+  {
+    name: "paunic",
+    role: "Developer",
+    avatar: "https://cdn.discordapp.com/avatars/852670185088221204/2bdae7c325dea23c874dbf234110718f.png?size=4096",
+    socials: {
+      discord: "https://discord.com/users/852670185088221204"
+    }
+  },
   },
   // Add more team members here
 ];
