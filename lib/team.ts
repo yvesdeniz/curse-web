8eb63f065cf73b00ee3ed2e1211aa0e982ba9b34
yvesdeniz <saint@afra.id>
2026-06-09 18:47:21 +0400

    feat: add latte to team page

--- lib/team.ts
+++ lib/team.ts
@@ -22,11 +22,18 @@ export const team: TeamMember[] = [
     },
   },
   {
+    name: "latte",
+    role: "Lead Catgirl & Developer",
+    avatar: "https://api.alo.ne/file/dovapq",
+    socials: {
+      discord: "https://discord.com/users/852670185088221204"
+    },
+  },
+  {
     name: "paunic",
     role: "Developer",
     avatar: "https://cdn.discordapp.com/avatars/852670185088221204/2bdae7c325dea23c874dbf234110718f.png?size=4096",
     socials: {
       discord: "https://discord.com/users/852670185088221204"
-    }
-  },
+    },
 ];
